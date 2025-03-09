c994417accc415a6ad028761f4af3179d6ba50b4
nferhat <nferhat20@gmail.com>
2025-03-09 12:39:46 +0100

    home/desktop: Use `programs.ghostty` instead of making my own generator

--- home/desktop/programs/ghostty.nix
+++ home/desktop/programs/ghostty.nix
@@ -5,59 +5,41 @@
 }: {
   home.packages = [inputs'.ghostty.packages.default];
 
-  # Simple configuration generator for ghostty
-  xdg.configFile.ghostty-config = {
-    target = "ghostty/config";
-    text = let
-      generateConfig = values:
-        builtins.concatStringsSep "\n" (lib.mapAttrsToList (key: value: "${key}=${
-            if value == false
-            then "false"
-            else if value == true
-            then "true"
-            else builtins.toString value
-          }")
-          values);
+  programs.ghostty = {
+    enable = true;
+    package = inputs'.ghostty.packages.default;
+    settings = let
       theme = import ../../../theme;
+      ansiColors = theme.ansi // theme.ansi-bright;
+      palette = lib.genList (i: let
+        colorValue = ansiColors."color${toString i}";
+      in "${toString i}=${colorValue}") 15;
+    in {
+      command = "fish";
+      font-family = "Fht Mono";
+      font-size = 10;
+      cursor-style = "block";
+      cursor-style-blink = false;
+      window-decoration = false;
+      bold-is-bright = false;
+      desktop-notifications = false; # annoying
+      auto-update = "off";
+      window-padding-balance = true;
+      adjust-box-thickness = 1;
+      adjust-underline-thickness = 1;
+      # adjust-underline-position = "100%";
+      # The heuristics to detected if ghostty should enable/disable this are not supported in
+      # fht-compositor, so we force this on instead.
+      gtk-single-instance = true;
+      # This does not really matter since all the serious work I do is under a tmux session
+      # Closing a terminal does not close the server.
+      confirm-close-surface = false;
 
-      values = {
-        command = "fish";
-        font-family = "Fht Mono";
-        font-size = 10;
-        cursor-style = "block";
-        cursor-style-blink = false;
-        window-decoration = false;
-        bold-is-bright = false;
-        desktop-notifications = false; # annoying
-        auto-update = "off";
-        window-padding-balance = true;
-        adjust-box-thickness = 1;
-        adjust-underline-thickness = 1;
-        # adjust-underline-position = "100%";
-        # The heuristics to detected if ghostty should enable/disable this are not supported in
-        # fht-compositor, so we force this on instead.
-        gtk-single-instance = true;
-        # This does not really matter since all the serious work I do is under a tmux session
-        # Closing a terminal does not close the server.
-        confirm-close-surface = false;
-
-        # Theme, directly ported from alacritty.
-        background = theme.background.primary;
-        background-opacity = 0.9;
-        foreground = theme.text.primary;
-      };
-
-      # In the https://www.ditig.com/publications/256-colors-cheat-sheet
-      # ansi colors come before their bright variant.
-      allAnsiPalette = theme.ansi // theme.ansi-bright;
-      palleteValues = builtins.listToAttrs (
-        builtins.map (idx: {
-          name = "palette=${builtins.toString idx}";
-          value = allAnsiPalette."color${builtins.toString idx}";
-        })
-        [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
-      );
-    in
-      generateConfig (values // palleteValues);
+      # Theme, directly ported from alacritty.
+      background = theme.background.primary;
+      background-opacity = 0.9;
+      foreground = theme.text.primary;
+      inherit palette;
+    };
   };
 }
--- home/thinkpad-t14s/default.nix
+++ home/thinkpad-t14s/default.nix
@@ -10,59 +10,6 @@
   # Yeah, 24 is way too small
   home.pointerCursor.size = lib.mkForce 48;
 
-  # FIXME: Avoid copying all of this? I just want a bigger font
-  # What would be better is having such generator included with ghostty flake upstream.
-  xdg.configFile.ghostty-config = lib.mkForce {
-    target = "ghostty/config";
-    text = let
-      generateConfig = values:
-        builtins.concatStringsSep "\n" (lib.mapAttrsToList (key: value: "${key}=${
-            if value == false
-            then "false"
-            else if value == true
-            then "true"
-            else builtins.toString value
-          }")
-          values);
-      theme = import ../../theme;
-
-      values = {
-        command = "fish";
-        font-family = "Fht Mono";
-        font-size = 13;
-        cursor-style = "block";
-        cursor-style-blink = false;
-        window-decoration = false;
-        bold-is-bright = false;
-        desktop-notifications = false; # annoying
-        auto-update = "off";
-        window-padding-balance = true;
-        adjust-box-thickness = 1;
-        adjust-underline-thickness = 1;
-        # The heuristics to detected if ghostty should enable/disable this are not supported in
-        # fht-compositor, so we force this on instead.
-        gtk-single-instance = true;
-        # This does not really matter since all the serious work I do is under a tmux session
-        # Closing a terminal does not close the server.
-        confirm-close-surface = false;
-
-        # Theme, directly ported from alacritty.
-        background = theme.background.primary;
-        background-opacity = 0.9;
-        foreground = theme.text.primary;
-      };
-
-      # In the https://www.ditig.com/publications/256-colors-cheat-sheet
-      # ansi colors come before their bright variant.
-      allAnsiPalette = theme.ansi // theme.ansi-bright;
-      palleteValues = builtins.listToAttrs (
-        builtins.map (idx: {
-          name = "palette=${builtins.toString idx}";
-          value = allAnsiPalette."color${builtins.toString idx}";
-        })
-        [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
-      );
-    in
-      generateConfig (values // palleteValues);
-  };
+  # 10 is adequate for the tiny 1366x768 screen I had on hp-da0018nk
+  programs.ghostty.settings.font-size = 13;
 }
