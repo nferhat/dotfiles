167855a3b030dd7a613e6ea4f9123159b0fd414d
nferhat <nferhat20@gmail.com>
2025-04-02 01:48:43 +0100

    home(thinkpad-t14s): Include monitor config

--- home/thinkpad-t14s/default.nix
+++ home/thinkpad-t14s/default.nix
@@ -5,6 +5,30 @@
       outer-gaps = lib.mkForce 30;
       inner-gaps = lib.mkForce 15;
     };
+
+    # This laptops gets wired up to a 2k MSI screen.
+    # On the thinkpad station with the DPI port I am using it registers as DP-4 and sometimes DP-5
+    # Annoying.
+    outputs = rec {
+      # The layout I am rocking with
+      # ---
+      #          +------------+
+      # +-------+|    DP-4/   |
+      # | eDP-1 ||    /DP-5   |
+      # +-------++------------+
+      eDP-1 = {
+        # Always put my laptop screen at the bottom left.
+        # S
+        position = [0 360];
+      };
+      DP-4 = {
+        position = [1920 0];
+        # The laptop cannot output 2k above 120hz
+        # And 1080 above 165, bandwidth limitation.
+        mode = "2560x1440@120";
+      };
+      DP-5 = DP-4;
+    };
   };
 
   # Yeah, 24 is way too small
