9b6568e4ac02b76aae4deb73745297ad4c50823c
nferhat <nferhat20@gmail.com>
2025-08-15 18:30:21 +0100

    config(nvim): stylua

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -92,7 +92,7 @@ local M = {
                 untracked = { text = "┃" },
             },
             signs_staged_enable = true,
-            signcolumn = true,         -- Toggle with `:Gitsigns toggle_signs`
+            signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
             current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
             current_line_blame_opts = { virt_text = true, virt_text_pos = "right_align" },
             current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
