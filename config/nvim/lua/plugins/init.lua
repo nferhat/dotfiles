86d06cbd1a4adef4aaa7b50f50b60d1d24b1e481
nferhat <nferhat20@gmail.com>
2025-08-15 18:29:51 +0100

    config(nvim): Remove `mini.comment`
    Neovim has now a comment plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -23,19 +23,6 @@ local M = {
     },
 
     {
-        "echasnovski/mini.comment",
-        keys = {
-            { "gcc", mode = "n", desc = "Comment toggle current line" },
-            { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-            { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-            { "gbc", mode = "n", desc = "Comment toggle current block" },
-            { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-            { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
-        },
-        config = true,
-    },
-
-    {
         "lukas-reineke/indent-blankline.nvim",
         event = "User FilePost",
         main = "ibl",
