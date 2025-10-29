82b3e1c1babf9a76f4e1c185d4c46cdb8777ac3e
nferhat <nferhat20@gmail.com>
2025-10-29 15:48:26 +0100

    config(nvim): Set `sw=2` in dart files

--- config/nvim/after/ftplugin/dart.lua
+++ config/nvim/after/ftplugin/dart.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>Fs", "<Cmd>FlutterSuper<CR>", { desc = "Go to super class" })
 vim.keymap.set("n", "<leader>Fo", "<Cmd>FlutterOutlineToggle<CR>", { desc = "Toggle outline" })
 vim.keymap.set("n", "<leader>Fr", "<Cmd>FlutterRename<CR>", { desc = "Rename class and imports" })
+vim.opt.sw = 2
