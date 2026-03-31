8b29d82da39e4a7d46f1950dfbf45632fb3d727b
nferhat <nadjib.ferhat@etu.usthb.dz>
2026-03-31 19:15:39 +0100

    hosts(desktop): Use helix+zed

--- hosts/shared/desktop.nix
+++ hosts/shared/desktop.nix
@@ -32,8 +32,8 @@
   };
 
   environment.sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "neovide";
+    EDITOR = "hx";
+    VISUAL = "zeditor";
     # XDG variables setup
     # safe defaults, in case the xdg nixos module, setting them here to avoid race conditions
     # as the xdg module sets them after `environment.variables` are set.
