dc16525abddf050d062886a5da1d6dc0f30211a4
nferhat <nadjib.ferhat@etu.usthb.dz>
2026-06-01 19:25:21 +0100

    home(desktop): Don't link quickshell config yet

--- config/quickshell/components/Shadow.qml
+++
@@ -1,8 +0,0 @@
-import QtQuick.Effects
-
-MultiEffect {
-    shadowEnabled: true
-    shadowBlur: 1
-    shadowOpacity: 1
-    shadowColor: "black"
-}
--- home/desktop/quickshell.nix
+++ home/desktop/quickshell.nix
@@ -7,7 +7,7 @@
     inputs'.quickshell.packages.default
     inputs'.fht-compositor-qml-plugin.packages.default
   ];
-  xdg.configFile."quickshell".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/nferhat/Documents/repos/personal/dotfiles/config/quickshell";
+  # xdg.configFile."quickshell".source =
+  #   config.lib.file.mkOutOfStoreSymlink "/home/nferhat/Documents/repos/personal/dotfiles/config/quickshell";
   # FIXME: Generate Colors.qml
 }
