bb2717a1f2021e287f5255790e3e648fa9ddc221
nferhat <nadjib.ferhat@etu.usthb.dz>
2026-08-20 13:18:08 +0100

    clean: Remove `flake-parts` from flake inputs

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
