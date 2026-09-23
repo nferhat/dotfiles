ef6c8ed5aff920a044890d024b9bb994213fdc18
nferhat <nadjib.ferhat@etu.usthb.dz>
2026-09-23 19:39:09 +0100

    modules(syncthing): Use HM's user syncthing service

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,10 +1,15 @@
-{ ... }: {
+{...}: {
   # Syncthing is mostly here to sync my notes and my password database.
   # Since I use KeePassXC, there's no sharing built-in.
-  services.syncthing = {
+  nferhat.services.syncthing = {
     enable = true;
-    openDefaultPorts = true;
     # FIXME: Perhaps declare some stuff to sync here instead of it being only set in the Web UI?
     # I dunno about that, you already know way too much about me with this config here.
+  };
+
+  # Open the ports needed for transfer and discovery
+  networking.firewall = {
+    allowedTCPPorts = [22000];
+    allowedUDPPorts = [21027 22000];
   };
 }
